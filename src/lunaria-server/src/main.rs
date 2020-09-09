624f4adf049b7ad71687bbc49f715199b9aebdf8
Jan David <jandavid@playlunaria.com>
2020-09-09 22:20:01 +0200

    Set up core loop with default plugins

--- src/lunaria-server/src/main.rs
+++ src/lunaria-server/src/main.rs
@@ -1,5 +1,15 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 
 fn main() {
-    App::build().run();
+    App::build()
+        .add_plugin(bevy::type_registry::TypeRegistryPlugin::default())
+        .add_plugin(bevy::core::CorePlugin::default())
+        .add_plugin(bevy::transform::TransformPlugin::default())
+        .add_plugin(bevy::diagnostic::DiagnosticsPlugin::default())
+        .add_plugin(bevy::app::ScheduleRunnerPlugin::run_loop(
+            Duration::from_secs_f64(1.0 / 60.0),
+        ))
+        .run();
 }
