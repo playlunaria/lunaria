fd94282a3da29e40d1069a2709ad5d8e7b413fac
Jan David <jandavid@playlunaria.com>
2020-09-12 22:01:36 +0200

    Implement unary gRPC calls with async channels

--- src/lunaria/src/grpc.rs
+++ src/lunaria/src/grpc.rs
@@ -1,35 +1,80 @@
-use bevy::app::{AppBuilder, Plugin};
-use tokio::task::JoinHandle;
-use tonic::transport::{Error, Server};
+use bevy::prelude::*;
+use std::sync::Arc;
+use tokio::sync::{mpsc, oneshot, Mutex};
+use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
-use lunaria_api::counter::count_service_server::{CountService, CountServiceServer};
+use lunaria_api::counter::count_service_server::CountServiceServer;
 use lunaria_api::counter::{GetCountRequest, GetCountResponse};
+
+use super::Counter;
 
 pub struct GrpcPlugin;
 
 impl Plugin for GrpcPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(initialize_grpc_server());
+        app.add_resource(initialize_grpc_server())
+            .add_system(process_requests_system.system());
     }
 }
 
-fn initialize_grpc_server() -> JoinHandle<Result<(), Error>> {
+fn initialize_grpc_server() -> GrpcQueue {
+    let (cmd_tx, cmd_rx) = mpsc::channel::<(GrpcEvent, oneshot::Sender<usize>)>(100);
+
     let addr = "0.0.0.0:10000".parse().unwrap();
 
-    let server = CountServiceServer::new(Counter);
+    let server = CountServiceServer::new(CounterService {
+        request_queue: Arc::new(Mutex::new(cmd_tx)),
+    });
 
-    tokio::spawn(Server::builder().add_service(server).serve(addr))
+    tokio::spawn(Server::builder().add_service(server).serve(addr));
+
+    GrpcQueue {
+        request_queue: cmd_rx,
+    }
 }
 
-struct Counter;
+fn process_requests_system(counter: Res<Counter>, mut queue: ResMut<GrpcQueue>) {
+    while let Ok((request, response)) = queue.request_queue.try_recv() {
+        match request {
+            GrpcEvent::GetCountRequest => {
+                response.send(counter.count).unwrap();
+            }
+        }
+    }
+}
+
+enum GrpcEvent {
+    GetCountRequest,
+}
+
+struct GrpcQueue {
+    request_queue: mpsc::Receiver<(GrpcEvent, oneshot::Sender<usize>)>,
+}
+
+type GrpcQueueSender = Arc<Mutex<mpsc::Sender<(GrpcEvent, oneshot::Sender<usize>)>>>;
+
+struct CounterService {
+    request_queue: GrpcQueueSender,
+}
 
 #[tonic::async_trait]
-impl CountService for Counter {
+impl lunaria_api::counter::count_service_server::CountService for CounterService {
     async fn get_count(
         &self,
         _request: Request<GetCountRequest>,
     ) -> Result<Response<GetCountResponse>, Status> {
-        Ok(Response::new(GetCountResponse { count: 0 }))
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        self.request_queue
+            .lock()
+            .await
+            .send((GrpcEvent::GetCountRequest, resp_tx))
+            .await
+            .ok()
+            .unwrap();
+        let res = resp_rx.await.unwrap();
+
+        Ok(Response::new(GetCountResponse { count: res as u64 }))
     }
 }
