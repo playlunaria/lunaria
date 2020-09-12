97ad272be30e4a49ac1654739fd5ceed8af6d69a
Jan David <jandavid@playlunaria.com>
2020-09-12 18:51:43 +0200

    Create counter resource to query from API

--- src/lunaria/src/main.rs
+++ src/lunaria/src/main.rs
@@ -1,5 +1,17 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_default_plugins().run();
+    App::build()
+        .add_default_plugins()
+        .add_resource(Counter { count: 0 })
+        .add_system(increase_counter_system.system())
+        .run();
+}
+
+struct Counter {
+    count: usize,
+}
+
+fn increase_counter_system(mut counter: ResMut<Counter>) {
+    counter.count += 1;
 }
