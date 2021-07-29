09550370c49c2d8f0dbc5946945ce960297bae5b
Jan David <jandavid@playlunaria.com>
2021-07-29 21:31:38 +0200

    Stream game status updates (#73)
    The new API to stream game status updates has been hooked up within the
    game. When a new game is started or stopped, a status update is streamed
    to connected clients. The engine listens to signals from Godot, and
    converts them to gRPC messages.

--- src/lunaria-godot/src/lib.rs
+++ src/lunaria-godot/src/lib.rs
@@ -1,12 +1,17 @@
 use gdnative::prelude::*;
 
 use crate::engine::EngineSingleton;
+use crate::scenes::game::GameScene;
 
 mod engine;
+mod scenes;
 
 fn init(handle: InitHandle) {
     // Singletons
     handle.add_class::<EngineSingleton>();
+
+    // Scenes
+    handle.add_class::<GameScene>();
 }
 
 godot_init!(init);
--- src/lunaria/src/lib.rs
+++ src/lunaria/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod api;
 pub mod engine;
 pub mod error;
+pub mod game;
