e954d6e81d13a47ecd4c704dd04df64eb6e3a600
Jan David <jandavid@playlunaria.com>
2021-08-22 10:21:18 +0200

    Update API crate and implement synchronous start game command
    The API crate has been bumped, and the API server has been updated to
    provide the synchronous start game command defined in the API specs. The
    command is sent to the engine using a command bus.

--- src/lunaria-godot/src/engine.rs
+++ src/lunaria-godot/src/engine.rs
@@ -1,18 +1,17 @@
 use gdnative::prelude::*;
-use tokio::sync::{mpsc, watch};
+use tokio::sync::{broadcast, watch};
 
 use lunaria::command::Command;
 use lunaria::engine::Engine;
 use lunaria::event::Event;
 
-type CommandQueue = mpsc::Receiver<Command>;
+type CommandQueue = broadcast::Receiver<Command>;
 type EventQueue = watch::Sender<Event>;
 
 #[derive(NativeClass)]
 #[inherit(Node)]
 pub struct EngineSingleton {
     _engine: Engine,
-    #[allow(dead_code)] // TODO: Remove when switching to synchronous start command
     command_queue: CommandQueue,
     event_queue: EventQueue,
 }
@@ -20,10 +19,10 @@ pub struct EngineSingleton {
 #[methods]
 impl EngineSingleton {
     fn new(_owner: &Node) -> Self {
-        let (command_sender, command_receiver) = mpsc::channel(256);
-        let (event_sender, event_receiver) = watch::channel(Event::None);
+        let (command_sender, command_receiver) = broadcast::channel(256);
+        let (event_sender, _event_receiver) = watch::channel(Event::None);
 
-        let engine = match Engine::new(command_sender, event_receiver) {
+        let engine = match Engine::new(command_sender) {
             Ok(engine) => engine,
             Err(error) => panic!("{}", error),
         };
@@ -41,6 +40,15 @@ impl EngineSingleton {
     }
 
     #[export]
+    fn _process(&mut self, owner: TRef<Node>, _delta: f32) {
+        while let Ok(command) = self.command_queue.try_recv() {
+            match command {
+                Command::StartGame => self.start_game(owner),
+            }
+        }
+    }
+
+    #[export]
     fn on_game_status_running(&self, _owner: TRef<Node>) {
         if self.event_queue.send(Event::GameStarted).is_err() {
             // TODO: Log error
@@ -51,6 +59,16 @@ impl EngineSingleton {
     fn on_game_status_stopped(&self, _owner: TRef<Node>) {
         if self.event_queue.send(Event::GameFinished).is_err() {
             // TODO: Log error
+        }
+    }
+
+    fn start_game(&self, owner: TRef<Node>) {
+        if let Some(tree_ref) = owner.get_tree() {
+            let tree = unsafe { tree_ref.assume_safe() };
+
+            if let Err(error) = tree.change_scene("res://scenes/game/Game.tscn") {
+                panic!("Failed to switch to game scene. {}", error);
+            }
         }
     }
 }
